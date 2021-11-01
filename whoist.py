bb02c1487205e57369bb3e744d80a97d8a23f4cf
Isaac Lovera <isaaclovera5@gmail.com>
2021-11-01 14:51:48 -0400

    whois

---
+++ whoist.py
@@ -0,0 +1,51 @@
+# 'domain_name':          r'Domain Name: *(.+)',
+#         'registrar':            r'Registrar: *(.+)',
+#         'whois_server':         r'Whois Server: *(.+)',
+#         'referral_url':         r'Referral URL: *(.+)',  # http url of whois_server
+#         'updated_date':         r'Updated Date: *(.+)',
+#         'creation_date':        r'Creation Date: *(.+)',
+#         'expiration_date':      r'Expir\w+ Date: *(.+)',
+#         'name_servers':         r'Name Server: *(.+)',  # list of name servers
+#         'status':               r'Status: *(.+)',  # list of statuses
+#         'emails':               EMAIL_REGEX,  # list of email s
+#         'dnssec':               r'dnssec: *([\S]+)',
+#         'name':                 r'Registrant Name: *(.+)',
+#         'org':                  r'Registrant\s*Organization: *(.+)',
+#         'address':              r'Registrant Street: *(.+)',
+#         'city':                 r'Registrant City: *(.+)',
+#         'state':                r'Registrant State/Province: *(.+)',
+#         'zipcode':              r'Registrant Postal Code: *(.+)',
+#         'country':              r'Registrant Country: *(.+)',
+
+import whois
+import os
+
+
+with open("dominios.txt", "r") as archivo:
+
+    for linea in archivo.read().splitlines():
+        try:
+            w = whois.whois(linea)
+            registrador = 'PDR Ltd. d/b/a PublicDomainRegistry.com'
+            whoiSer = 'whois.publicdomainregistry.com'
+
+            serverWhois = w.whois_server
+            serverWhois = w.whois_server
+            expiracion = w.expiration_date
+            creacion = w.creation_date
+            dominio = w.name
+            destFile = r"act.txt"
+            if serverWhois == whoiSer:
+                destFile = r"act.txt"
+                with open(destFile, 'a') as f:
+                    f.write(linea+"\n")
+                # print('true' + " " + linea)
+            else:
+                destFile = r"noact.txt"
+                with open(destFile, 'a') as f:
+                    f.write(linea+"\n")
+            # print (str(serverWhois + " " + linea))
+        except:
+            destFile = r"deleteDomain.txt"
+            with open(destFile, 'a') as f:
+                f.write(linea+"\n")
